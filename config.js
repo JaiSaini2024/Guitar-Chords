303d9576104aad06eb94ebb33495b2180c2363d9
RAHUL <33372792+JaiSaini2024@users.noreply.github.com>
2026-04-22 18:32:37 +0530

    update

---
+++ config.js
@@ -0,0 +1,18 @@
+// js/config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAz3iD45chxwHfGcAnfmx7V3jWYLfcXZOU",
+    authDomain: "rating-c371c.firebaseapp.com",
+    databaseURL: "https://rating-c371c-default-rtdb.firebaseio.com",
+    projectId: "rating-c371c",
+    storageBucket: "rating-c371c.appspot.com",
+    messagingSenderId: "214609855829",
+    appId: "1:214609855829:web:05a8e678bb209087c1dbdc"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+window.appAuth = firebase.auth();
+window.appDb = firebase.database();
+window.appProvider = new firebase.auth.GoogleAuthProvider();
